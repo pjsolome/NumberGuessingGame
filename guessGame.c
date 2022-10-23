86c55da853defa777c07637b8784201fd391bc30
Parker Solome <pjsolome@cpp.edu>
2022-10-23 16:13:26 -0700

    Initial commit

---
+++ guessGame.c
@@ -0,0 +1,27 @@
+//Display main menu
+
+//Choose to play a game
+
+//Program chooses random number
+
+//User enters number
+
+//Loop to keep player in the game till they get the right number
+
+//Check if number is between 1 and 10
+
+//Compare user number to random number
+
+//Tells user higher or lower
+
+//Tells user they got the right number
+
+//Pressing 'q' quits game
+
+//Set a new max number; tell the user it can not go over 10
+
+//Make sure new max is not negative
+
+//Make sure new max is not more than 10
+
+//Quit the program
